2fbed0c543e8bee05f5c3add29ede4c32cab5a84
Preston Hale <phale@firststrikegames.com>
2021-04-27 23:50:54 -0700

    Better table shapes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io::{stdin,stdout,Write};
 use std::collections::HashMap;
+use std::vec::Vec;
 use uuid::Uuid;
 
 use rand;
 use rand::seq::SliceRandom;
 
-use rusoto_dynamodb::{DynamoDbClient, DynamoDb, PutItemInput, GetItemInput, QueryInput, UpdateItemInput, AttributeValue};
+use rusoto_dynamodb::{DynamoDbClient, DynamoDb, PutItemInput, GetItemInput, ScanInput, QueryInput, UpdateItemInput, AttributeValue};
 use rusoto_core::{Region};
 
 fn main() {
@@ -39,45 +40,39 @@ async fn write_message(user_id: &String){
     let message_id = Uuid::new_v4();
 
     // Update the player's list of available messages
-    let mut key = HashMap::new();
-    key.insert(
-        String::from("PK"),
-        AttributeValue{
-            s: Some(format!("USER#{}", user_id.clone())),
-            ..Default::default()
-        },
-    );
-    key.insert(
-        String::from("SK"),
-        AttributeValue{
-            s: Some(format!("MSG#{}", message_id.to_string())),
-            ..Default::default()
-        },
-    );
-    let mut expression_attribute_values = HashMap::new();
-    expression_attribute_values.insert(
-        String::from(":new_message"),
-        AttributeValue{
-            s: Some(input.to_string()),
-            ..Default::default()
-        },
-    );
-    expression_attribute_values.insert(
-        String::from(":read"),
-        AttributeValue{
-            s: Some(String::from("f")),
-            ..Default::default()
-        },
-    );
-    let update_input = UpdateItemInput{
-        key: key,
-        update_expression: Some(String::from("
-            SET message = :new_message, GSI1PK = :read")),
-        expression_attribute_values: Some(expression_attribute_values),
+    let item: HashMap<String, AttributeValue> = [
+        (
+            String::from("user_id"),
+            AttributeValue{
+                s: Some(String::from(user_id)),
+                ..Default::default()
+            }
+        ), (
+            String::from("message_id"),
+            AttributeValue{
+                s: Some(format!("{}", message_id)),
+                ..Default::default()
+            }
+        ), (
+            String::from("message"),
+            AttributeValue{
+                s: Some(input),
+                ..Default::default()
+            }
+        ), (
+            String::from("unread"),
+            AttributeValue{
+                s: Some(String::from("t")),
+                ..Default::default()
+            }
+        )
+    ].iter().cloned().collect();
+    let put_input = PutItemInput{
+        item: item,
         table_name: String::from("messages"),
         ..Default::default()
     };
-    match dynamo_client.update_item(update_input).await {
+    match dynamo_client.put_item(put_input).await {
         Ok(_) => println!("Message sent!"),
         Err(error) => {
             println!("Error: {:?}", error);
@@ -89,72 +84,110 @@ async fn write_message(user_id: &String){
 async fn get_message(user_id: &String){
     let dynamo_client = DynamoDbClient::new(Region::UsWest2);
 
-    // TODO: Get users the player has a relationship with and/or random players
-    // Select a message from those users to deliver
-    let mut selected_message_id: String = String::from("");
-    let mut expression_attribute_values = HashMap::new();
-    expression_attribute_values.insert(
-        String::from(":user_id"),
-        AttributeValue{
-            s: Some(String::from("bob")), // TODO: Hardcoded
-            ..Default::default()
+    let mut selected_message_id = "".to_string();
+    let mut selected_user_id = "".to_string();
+
+    // Find user with a relationship
+    let mut relationships = vec![];
+    let key: HashMap<String, AttributeValue> = [
+        (
+            "user_id".to_string(),
+            AttributeValue{
+                s: Some(user_id.to_string()) ,
+                ..Default::default()
+            }
+        )
+    ].iter().cloned().collect();
+    let get_item_input = GetItemInput{
+        key: key,
+        table_name: String::from("users"),
+        ..Default::default()
+    };
+    match dynamo_client.get_item(get_item_input).await {
+        Ok(output) => match output.item{
+            Some(item) => {
+                let item_relationships = item["relationships"].l.as_ref().unwrap();
+                for r in item_relationships.iter(){
+                    let relationship = r.m.as_ref().unwrap();
+                    relationships.push(relationship["user_id"].s.as_ref().unwrap().clone());
+                }
+            },
+            None => println!("Something went wrong"),
+        },
+        Err(error) => {
+            println!("Error: {:?}", error);
         }
-    );
+    };
+
+    // Get a random related user
+    let chosen_user  = relationships.choose(&mut rand::thread_rng()).unwrap();
+
+    // Get all unread messages from selected user
+    let expression_attribute_values: HashMap<String, AttributeValue> = [
+        (
+            ":user_id".to_string(),
+            AttributeValue{
+                s: Some(chosen_user.to_string()),
+                ..Default::default()
+            }
+        )
+    ].iter().cloned().collect();
     let query_input = QueryInput{
         expression_attribute_values: Some(expression_attribute_values),
-        key_condition_expression: Some(format!("user_id = :user_id")),
-        table_name: String::from("available_messages"),
+        key_condition_expression: Some("user_id = :user_id".to_string()),
+        index_name: Some(String::from("GSI1")),
+        table_name: String::from("messages"),
         ..Default::default()
     };
     match dynamo_client.query(query_input).await {
         Ok(output) => match output.items{
             Some(items) => {
-                for x in items.iter(){
-                    let messages = x["available_messages"].ss.as_ref().expect("Available_messages is not a set?");
-                    let selected = messages.choose(&mut rand::thread_rng()).expect("Random msg selection failed.");
-                    selected_message_id = String::from(selected);
+                match items.choose(&mut rand::thread_rng()) {
+                    Some(selected) => {
+                        println!("{}", selected["message"].s.as_ref().unwrap());
+                        selected_user_id = selected["user_id"].s.as_ref().unwrap().clone();
+                        selected_message_id = selected["message_id"].s.as_ref().unwrap().clone();
+                    }
+                    None => println!("No messages available"),
                 }
-            },
-            None => println!("No mail yet!"),
-        }
+            }
+            None => println!("Something went wrong"),
+        },
         Err(error) => {
             println!("Error: {:?}", error);
         }
     };
 
-    if selected_message_id == ""{
-        return
+    if selected_message_id == "" || selected_user_id == "" {
+        println!("No valid message found!");
+        return;
     }
 
-    // Deliver the message
-    let mut key = HashMap::new();
-    key.insert(
+    // Mark a message as read
+    let key: HashMap<String, AttributeValue> = [(
+        String::from("user_id"),
+        AttributeValue{
+            s: Some(selected_user_id.to_string()),
+            ..Default::default()
+        }), (
         String::from("message_id"),
         AttributeValue{
             s: Some(selected_message_id.to_string()),
             ..Default::default()
         }
-    );
-    let get_input = GetItemInput{
+    )].iter().cloned().collect();
+    let update_input = UpdateItemInput{
         key: key,
+        update_expression: Some("REMOVE unread".to_string()),
         table_name: String::from("messages"),
         ..Default::default()
     };
-    match dynamo_client.get_item(get_input).await {
-        Ok(output) => match output.item{
-            Some(item) => {
-                let message = item["message"].s.as_ref().expect("Message is not string type?");
-                println!("{}", message)
-            },
-            None => println!("Oh no! Failed to find message! This shouldn't happen."),
-        }
+    match dynamo_client.update_item(update_input).await {
+        Ok(output) => println!("Read"),
         Err(error) => {
             println!("Error: {:?}", error);
         }
-    };
-
-    // TODO: Mark the delivered message as read
-    // by REMOVEing it from the "available_messages" table
+    }
 }
 
 fn get_input(prompt: String) -> String {
